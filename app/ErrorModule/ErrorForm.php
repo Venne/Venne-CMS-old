4e5f1824f1a0c39eee29413ee8e36e9df7a6d756
Josef <pepa@myhost.(none)>
2011-09-12 10:01:47 +0200

    added new modules

---
+++ app/ErrorModule/ErrorForm.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Venne:CMS (version 2.0-dev released on $WCDATE$)
+ *
+ * Copyright (c) 2011 Josef Kříž pepakriz@gmail.com
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace Venne\ErrorModule;
+
+use Venne\ORM\Column;
+use Nette\Utils\Html;
+use Venne\Forms\Form;
+
+/**
+ * @author Josef Kříž
+ */
+class ErrorForm extends \Venne\Developer\Form\EditForm {
+
+	public function startup()
+	{
+		parent::startup();
+
+		$this->addGroup();
+		if (!$this->key) {
+			$this->addText("code", "Code")->addRule(self::FILLED, "Enter code");
+		}
+		$this->addTextArea("text", "Text");
+	}
+
+
+	public function load()
+	{
+		$values = $this->presenter->context->services->error->getError($this->key);
+
+		$this["text"]->setValue($values->text);
+	}
+
+
+	public function save()
+	{
+		parent::save();
+		$values = $this->getValues();
+		$model = $this->presenter->context->services->error;
+
+		if ($this->key) {
+			$values["code"] = $model->getError($this->id)->code;
+		}
+
+		$model->saveError($values["code"], $values["text"]);
+	}
+
+}
---
+++ app/PagesModule/PagesEntity.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Venne:CMS (version 2.0-dev released on $WCDATE$)
+ *
+ * Copyright (c) 2011 Josef Kříž pepakriz@gmail.com
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace PagesModule;
+
+/**
+ * @author Josef Kříž
+ * @Entity(repositoryClass="Venne\Developer\Doctrine\BaseRepository")
+ * @Table(name="pages")
+ * 
+ * @property $mainPage
+ * @property $title
+ * @property $keywords
+ * @property $description
+ * @property $text
+ * @property $created
+ * @property $updated
+ * @property $website
+ * @property $url
+ */
+class PagesEntity extends \Venne\Developer\Doctrine\BaseEntity{
+	
+	public function __construct()
+	{
+		$this->created = new \Nette\DateTime;
+		$this->updated = new \Nette\DateTime;
+		$this->mainPage = false;
+	}
+
+
+	/**
+	 * @Column(type="string")
+	 * 
+	 * @Form(group="Item")
+	 */
+	protected $title;
+	
+	/**
+	 * @Column(type="string")
+	 * 
+	 * @Form
+	 */
+	protected $keywords;
+	
+	/**
+	 * @Column(type="string")
+	 * 
+	 * @Form
+	 */
+	protected $description;
+	
+	/**
+	 * @Column(type="text")
+	 * 
+	 * @Form
+	 */
+	protected $text;
+	
+	/**
+	 * @Column(type="datetime")
+	 */
+	protected $created;
+	
+	/**
+	 * @Column(type="datetime")
+	 */
+	protected $updated;
+	
+	/**
+	 * @Column(type="string")
+	 */
+	protected $url;
+	
+	/**
+	 * @Column(type="boolean")
+	 */
+	protected $mainPage;
+	
+}
