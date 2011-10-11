734e18356f62cbf1b275b69f1b0fc88fec6a8856
Josef <pepa@myhost.(none)>
2011-10-11 16:47:59 +0200

    ModulesModule: fixed getLayouts

--- libs/Venne/Modules/Modules/Service.php
+++ libs/Venne/Modules/Modules/Service.php
@@ -227,7 +227,7 @@ class Service extends \Venne\Developer\Service\BaseService {
 	public function getLayouts()
 	{
 		$data = array();
-		foreach (\Nette\Utils\Finder::findFiles("@*.latte")->in($this->context->params["wwwDir"] . "/themes/" . $this->context->params["venne"]["website"]["template"] . "/layouts/") as $file) {
+		foreach (\Nette\Utils\Finder::findFiles("@*.latte")->in($this->context->params["wwwDir"] . "/themes/" . $this->context->params["venne"]["website"]["theme"] . "/layouts/") as $file) {
 			$data[substr($file->getBaseName(), 1, -6)] = substr($file->getBaseName(), 1, -6);
 		}
 		return $data;
