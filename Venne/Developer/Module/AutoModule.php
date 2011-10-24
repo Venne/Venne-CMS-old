ff32a368afb3cb917535a5025a323bd261bf9d23
Josef Kříž <pepakriz@gmail.com>
2011-10-24 14:17:59 +0200

    AutoModule: added cache

--- Venne/Developer/Module/AutoModule.php
+++ Venne/Developer/Module/AutoModule.php
@@ -21,44 +21,48 @@ abstract class AutoModule extends BaseModule {
 	{
 		parent::setPermissions($container, $permissions);
 
-		$resources = array();
+		$cache = new \Nette\Caching\Cache($container->cacheStorage, "Venne.AutoModule");
+		$resources = $cache->load($this->getName());
+		if ($resources === NULL) {
+			$resources = array();
 
-		$paths = array(
-			$container->params["appDir"] . "/" . ucfirst($this->getName()) . "Module",
-			$container->params["libsDir"] . "/App/" . ucfirst($this->getName()) . "Module"
-		);
+			$paths = array(
+				$container->params["appDir"] . "/" . ucfirst($this->getName()) . "Module",
+				$container->params["libsDir"] . "/App/" . ucfirst($this->getName()) . "Module"
+			);
 
 
-		foreach ($paths as $path) {
-			if (file_exists($path)) {
-				foreach (array("" => $path . "/presenters", "\\AdminModule" => $path . "/AdminModule/presenters") as $key => $pathP) {
-					if (file_exists($pathP)) {
-						foreach (\Nette\Utils\Finder::findFiles("*Presenter.php")->in($pathP) as $file) {
+			foreach ($paths as $path) {
+				if (file_exists($path)) {
+					foreach (array("" => $path . "/presenters", "\\AdminModule" => $path . "/AdminModule/presenters") as $key => $pathP) {
+						if (file_exists($pathP)) {
+							foreach (\Nette\Utils\Finder::findFiles("*Presenter.php")->in($pathP) as $file) {
 
-							$className = "\\App\\" . ucfirst($this->getName()) . "Module{$key}\\" . substr($file->getBaseName(), 0, -4);
+								$className = "\\App\\" . ucfirst($this->getName()) . "Module{$key}\\" . substr($file->getBaseName(), 0, -4);
 
-							$ref = new \Nette\Reflection\ClassType($className);
-							if ($ref->hasAnnotation("resource")) {
-								$resource = $ref->getAnnotation("resource");
-								$j = explode("\\", $resource);
-								unset($j[count($j) - 1]);
-								$j = join("\\", $j);
+								$ref = new \Nette\Reflection\ClassType($className);
+								if ($ref->hasAnnotation("resource")) {
+									$resource = $ref->getAnnotation("resource");
+									$j = explode("\\", $resource);
+									unset($j[count($j) - 1]);
+									$j = join("\\", $j);
 
-								$resources[] = array("resource" => $resource, "privilege" => NULL, "parent" => $j);
-							}
+									$resources[] = array("resource" => $resource, "privilege" => NULL, "parent" => $j);
+								}
 
-							foreach ($ref->getMethods() as $method) {
-								if ($method->hasAnnotation("privilege")) {
-									$resources[] = array("resource" => $resource, "privilege" => $method->getAnnotation("privilege"), "parent" => NULL);
+								foreach ($ref->getMethods() as $method) {
+									if ($method->hasAnnotation("privilege")) {
+										$resources[] = array("resource" => $resource, "privilege" => $method->getAnnotation("privilege"), "parent" => NULL);
+									}
 								}
 							}
 						}
 					}
 				}
 			}
+			sort($resources);
+			$cache->save($this->getName(), $resources);
 		}
-
-		sort($resources);
 
 		foreach ($resources as $item) {
 			if (!$permissions->hasResource($item["resource"])) {
